80addc2a69d073b8d5d465864f77ce23b9a41b6e
GiovaniFZ <giovanifinazzi@gmail.com>
2026-04-03 12:01:07 -0300

    feat: send jokes as string instead of object.

--- src/commands/joke.rs
+++ src/commands/joke.rs
@@ -1,25 +1,52 @@
+use serde::Deserialize;
 use teloxide::prelude::*;
 use teloxide::types::ReplyParameters;
 
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+enum JokeApiResponse {
+    #[serde(rename = "single")]
+    Single { joke: String },
+    #[serde(rename = "twopart")]
+    TwoPart { setup: String, delivery: String },
+}
 
 pub async fn run(bot: Bot, msg: Message) -> ResponseResult<()> {
     let url = "https://v2.jokeapi.dev/joke/Any";
-    let res = match reqwest::get(url).await {
-        Ok(v) => match v.text().await {
-            Ok(t) => t,
-            e =>{
+    let joke = match reqwest::get(url).await {
+        Ok(v) => match v.json::<JokeApiResponse>().await {
+            Ok(t) => Some(t),
+            Err(e) => {
                 println!("{e:#?}");
-                "err".to_string()
+                None
             }
         },
-        e => {
+        Err(e) => {
             println!("{e:#?}");
-            "err".to_string()
+            None
         }
     };
-    println!("{:#?}", res);
-    bot.send_message(msg.chat.id, res)
-        .reply_parameters(ReplyParameters::new(msg.id))
-        .await?;
+
+    match joke {
+        Some(JokeApiResponse::Single { joke }) => {
+            bot.send_message(msg.chat.id, joke)
+                .reply_parameters(ReplyParameters::new(msg.id))
+                .await?;
+        }
+        Some(JokeApiResponse::TwoPart { setup, delivery }) => {
+            bot.send_message(msg.chat.id, setup)
+                .reply_parameters(ReplyParameters::new(msg.id))
+                .await?;
+            bot.send_message(msg.chat.id, delivery)
+                .reply_parameters(ReplyParameters::new(msg.id))
+                .await?;
+        }
+        None => {
+            bot.send_message(msg.chat.id, "No joke found :(")
+                .reply_parameters(ReplyParameters::new(msg.id))
+                .await?;
+        }
+    }
+
     Ok(())
 }
