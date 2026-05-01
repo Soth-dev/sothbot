10389ff3012e8d900d257eaa6bfd9610cb417f9e
Soth-dev <mcbedrockplayer2008@gmail.com>
2026-05-01 10:28:36 +0700

    updated /ai and use gemini 3 flash

--- src/commands/ai.rs
+++ src/commands/ai.rs
@@ -1,8 +1,10 @@
-use gemini_rust::{ClientError::BadResponse, Gemini};
-use std::{env, sync::OnceLock};
+use gemini_rust::{ClientError::BadResponse, Gemini, Model};
+use std::{env, sync::LazyLock};
 use teloxide::{prelude::*, types::ReplyParameters};
 
-static GEMINI_CLIENT: OnceLock<Option<Gemini>> = OnceLock::new();
+static GEMINI_CLIENT: LazyLock<Gemini> = LazyLock::new(|| {
+    Gemini::with_model(env::var("GEMINI_API_KEY").unwrap(), Model::Gemini3Flash).unwrap()
+});
 
 pub async fn run(bot: Bot, msg: Message) -> ResponseResult<()> {
     let text = msg.text().unwrap_or("").replacen("/ai", "", 1);
@@ -12,30 +14,32 @@ pub async fn run(bot: Bot, msg: Message) -> ResponseResult<()> {
             .await?;
         return Ok(());
     }
+    let reply_text = match msg.reply_to_message() {
+        Some(m) => m.text(),
+        None => None,
+    };
 
-    let response = match GEMINI_CLIENT
-        .get_or_init(|| Gemini::new(env::var("GEMINI_API_KEY").unwrap_or("null".to_string())).ok())
-    {
-        Some(client) => match client
-            .generate_content()
-            .with_user_message(text)
-            .execute()
-            .await
-        {
-            Ok(t) => t.text(),
-            Err(e) => {
-                if let BadResponse { description, .. } = e {
-                    println!(
-                        "{}",
-                        description
-                            .as_deref()
-                            .unwrap_or("Error: \x1b[91m(No details)\x1b[0m")
-                    )
-                };
-                "failed to request".to_string()
-            }
-        },
-        None => "api key err".to_string(),
+    let mut content = GEMINI_CLIENT
+        .generate_content()
+        .with_user_message(text.trim());
+    content = match reply_text {
+        Some(t) => content.with_system_instruction(t),
+        None => content,
+    };
+
+    let response = match content.execute().await {
+        Ok(t) => t.text(),
+        Err(e) => {
+            if let BadResponse { description, .. } = e {
+                println!(
+                    "{}",
+                    description
+                        .as_deref()
+                        .unwrap_or("Error: \x1b[91m(No details)\x1b[0m")
+                )
+            };
+            "failed to request".to_string()
+        }
     };
 
     bot.send_message(msg.chat.id, response)
